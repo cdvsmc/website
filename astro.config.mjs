35c8556f794ad669305ca80f1150a7ebb5d2b80f
cdvsmc <215961361+cdvsmc@users.noreply.github.com>
2025-06-28 14:07:56 -0600

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,48 @@ export default defineConfig({
 	base: "/website",
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			//title: 'My Docs',
+			title: {
+				es: 'Mis doctos',
+				en: 'My Docs',
+			},
+			//Añadir un lugo personalizado
+			logo: {
+				src: './src/assets/houston.webp',
+				//light: './src/assets/houston.webp',
+				//dark: './src/assets/houston.webp',
+				replacesTitle: false,
+			},
+			// Configuración pára un sitio monolingüe.
+			// Consulte https://starlight.astro.build/es/guides/i18n/
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'Español',
+					lang:'es',
+				},
+			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight'},
+				{ icon: 'discord', label: 'Discord', href: 'https://astro.build/chat'},
+				{ icon: 'youtube', label: 'YouTube', href: 'https://www.youtube.com/@astrodotbuild'}
+			],
 			sidebar: [
 				{
-					label: 'Guides',
-					//items: [{ label: 'Example Guide', slug: 'guides/example' },],
-					autogenerate: { directory: 'guides' },
+					label: 'Guías',
+					translations: { es:'Guías' },
+					items: [
+						// Cada elemento aquí es una entrada en el menú de navegación.
+						{
+							label: 'Example Guide',	translations: { es: 'Guía de ejemplo' },
+							slug: 'guias/ejemplo'
+						},
+					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Referencia',
+					translations: { es: 'Referencia' },
+					autogenerate: { directory: 'referencia' },
 				},
 			],
 		}),
