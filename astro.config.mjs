39e16f6d906e992884bafab693325ba5b9a6e9f9
cdvsmc <215961361+cdvsmc@users.noreply.github.com>
2025-06-28 12:24:47 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://cdvsmc.github.io",
-	base: "website",
+	base: "/website",
 	integrations: [
 		starlight({
 			title: 'My Docs',
